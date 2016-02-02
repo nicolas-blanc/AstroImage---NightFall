412bddca9da6a540b366a14822b1f2d042aa54a8
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2016-02-02 15:12:53 +0100

    Add class Model

---
+++ src/model/Model.py
@@ -0,0 +1,11 @@
+
+
+class Model(object):
+    """docstring for Model"""
+    def __init__(self):
+        super(Model, self).__init__()
+        self.m_process = []
+        self.m_masterDark = null
+        self.m_masterFlat = null
+        self.m_masterOffset = null
+        self.m_light = null
