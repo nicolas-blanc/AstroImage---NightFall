83c0222b6b9ab9aebd202e3fa92b13262c23381f
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2016-02-16 13:50:06 +0100

    Change in Image.py

--- src/model/Image.py
+++ src/model/Image.py
@@ -19,16 +19,13 @@ class Image(object):
     """docstring for Image"""
     def __init__(self, path):
         super(Image, self).__init__()
+        print('path : ' + path)
         self.path = path
 
 
     # To get back the size of the image
     def getSize(self):
-    	return (self.height, self.width)
-
-
-    def getFile(self):
-        return self.file
+    	return (self._height, self._width)
 
 
 
@@ -39,7 +36,7 @@ class ImageRaw(Image):
     def __init__(self, path):
         super(ImageRaw, self).__init__(path)
         # Load a RAW file
-        self.file = rawpy.imread(path)
+        self._file = rawpy.imread(_path)
         # Erreur sur cette ligne, me dit que size n'existe pas
     	# self.raw_height, self.raw_width, self.height, self.width, self.top_margin, self.left_margin, self.iheight, self.iwidth, self.pixel_aspect, self.flip = self.file.size
 
@@ -47,11 +44,15 @@ class ImageRaw(Image):
     # Postprocess this file to obtain a numpy ndarray of shape (h,w,c)
     # 16 bits => la palette de couleur peut contenir 2^16 = 65536 couleurs
     def debayeurization(self):
-    	self.img = self.file.postprocess(output_bps=16, output_color=ColorSpace.sRGB, demosaic_algorithm=DemosaicAlgorithm.AAHD, use_camera_wb=True,no_auto_bright=True)
+    	self._img = self._file.postprocess(output_bps=16, output_color=ColorSpace.sRGB, demosaic_algorithm=DemosaicAlgorithm.AAHD, use_camera_wb=True, no_auto_bright=True)
 
 
     def getImageDebayeurization(self):
-        return self.img
+        return self._img
+
+
+    def getFile(self):
+        return self._file
 
 
 
@@ -63,44 +64,47 @@ class ImageFits(Image):
 
 
     # Opening FITS files and loading the image data
-    def readFITS(self, path):
-    	path = path
-    	hdu_list = pyfits.open(path, uint=True, do_not_scale_image_data=False) # returns an object called an HDUList which is a list-like collection of HDU objects.
-    	image_data = hdu_list[0].data # hdulist[0] is the primary HDU, hdulist[1] is the first extension HDU, etc. The data attribute of the HDU object will return a numpy ndarray object.
-    	hdu_list.close() # the headers will still be accessible after the HDUList is closed
-    	return (hdu_list, image_data)
+    def readFITS(self):
+    	self._hduList = pyfits.open(self._path, uint=True, do_not_scale_image_data=False) # returns an object called an HDUList which is a list-like collection of HDU objects.
+    	self._imageData = self._hduList[0].data # hdulist[0] is the primary HDU, hdulist[1] is the first extension HDU, etc. The data attribute of the HDU object will return a numpy ndarray object.
+    	self._hduList.close() # the headers will still be accessible after the HDUList is closed
 
-    #hdu,image_data_red, image_data_green, image_data_blue = readFITS('M13_blue_0001.fits')
+    #hdu,imageDataRed, imageDataGreen, imageDataBlue = readFITS('M13_blue_0001.fits')
     #print(hdu.info())
 
 
     # Convert separate FITS images (RGB) to 3-color array (nparray)
-    def ConvertFITS(self, fitsPath_red, fitsPath_green, fitsPath_blue):
-    	hdu_list_red, image_data_red = readFITS(fitsPath_red)
-    	hdu_list_green, image_data_green = readFITS(fitsPath_green)
-    	hdu_list_blue, image_data_blue = readFITS(fitsPath_blue)
-    	height,width = image_data_red.shape
-    	dataType = image_data_red.dtype.name
+    # Need fitsPathRed, fitsPathGreen, fitsPathBlue : each file is inside a folder give by the path
+    # Name of the file : red.fits / green.fits / blue.fits
+    def convertFITSToRGB(self):
+        fitsPathRed = self._path + "red.fits"
+        fitsPathGreen = self._path + "green.fits"
+        fitsPathBlue = self._path + "blue.fits"
+
+    	hduListRed, imageDataRed = readFITS(fitsPathRed)
+    	hduListGreen, imageDataGreen = readFITS(fitsPathGreen)
+    	hduListBlue, imageDataBlue = readFITS(fitsPathBlue)
+    	height,width = imageDataRed.shape
+    	dataType = imageDataRed.dtype.name
     	#print('')
     	#print('DATATYPE')
     	#print(dataType)
-    	rgbArray = np.empty(shape=(height,width,3),dtype=dataType)
-    	rgbArray[:,:,0] = image_data_red
-    	rgbArray[:,:,1] = image_data_green
-    	rgbArray[:,:,2] = image_data_blue
+    	self._rgbArray = np.empty(shape=(height,width,3),dtype=dataType)
+    	self._rgbArray[:,:,0] = imageDataRed
+    	self._rgbArray[:,:,1] = imageDataGreen
+    	self._rgbArray[:,:,2] = imageDataBlue
     	#print('ConvertFIFTS')
     	#print(rgbArray[:,:,0])
-    	return rgbArray
 
 
     # Converting a 3-color array to separate FITS images
-    def ConvertToFITS(self, rgbArray, fitsName, LATOBS='Not informed', LONGOBS='Not informed') :
-
-    	npr = rgbArray[:,:,0]
+    def convertRGBToFITS(self, rgbArray, fitsName, LATOBS='Not informed', LONGOBS='Not informed'):
+        self._rgbArray = rgbArray
+    	npr = self._rgbArray[:,:,0]
     	#print("CONVERT TO FITS")
     	#print(npr)
-    	npg = rgbArray[:,:,1]
-    	npb = rgbArray[:,:,2]
+    	npg = self._rgbArray[:,:,1]
+    	npb = self._rgbArray[:,:,2]
 
     	red = pyfits.PrimaryHDU()
     	red.header['LATOBS'] = LATOBS
@@ -122,6 +126,12 @@ class ImageFits(Image):
     	blue.data = npb
     	blue.writeto(fitsName+'_blue.fits')
 
+    def getRGBArray(self):
+        return self._rgbArray
+
+    def getHduListImageData(self, arg):
+        return (self._hduList, self._imageData)
+
 
 
 #---------- TESTS -------------#
@@ -132,12 +142,16 @@ class ImageFits(Image):
 #file = loadRaw(path)
 #array = debayeurization(file)
 
-#ConvertToFITS(array,'test')
-#rgbArray = ConvertFITS('test_red.fits', 'test_green.fits', 'test_blue.fits')
-#imageio.imsave('testFITS.tiff', rgbArray)
+
 
 if __name__ == '__main__':
-    path = '../../Pictures_test/DSC_0599.NEF'
-    iR1 = ImageRaw(path)
+    path = '../../Pictures_test/'
+    iR1 = ImageRaw(path + 'DSC_0599.NEF')
     iR1.debayeurization()
     imageio.imsave('../../Pictures_test/testFITS.tiff', iR1.getImageDebayeurization())
+
+    # iF1 = ImageFits(path)
+    # iF2 = ImageFits(path + 'FITS/')
+    # # iF1.convertRGBToFITS(iR1.getImageDebayeurization(),'test')
+    # iF2.convertFITSToRGB()
+    # imageio.imsave('testFITS.tiff', iF2.getRGBArray)
